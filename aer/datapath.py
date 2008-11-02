470057c04ff343c79bc7dd95dd33f2ab0b0a5bdd
gene <geneshuman@gmail.com>
2008-11-02 14:56:56 -0800

    ready to change textures

--- aer/datapath.py
+++ aer/datapath.py
@@ -5,14 +5,14 @@ def linear_1d(t, data):
     if(t > 1):
         t = 1
         raise "done"
-    return data['s'] * (1 - t) + data['e'] * t
+    return (data['s'] * (1 - t) + data['e'] * t, t != 1)
 
 
 def linear_2d(t, data):
     if(t > 1):
         t = 1
-    return complex(data['s'].real * (1 - t) + data['e'].real * t,
-                   data['s'].imag * (1 - t) + data['e'].imag * t)
+    return (complex(data['s'].real * (1 - t) + data['e'].real * t,
+                    data['s'].imag * (1 - t) + data['e'].imag * t), t != 1)
 
 
 def radial_2d(t, data):
