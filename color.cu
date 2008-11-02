3de182341f18f3019e520415f49be682fbb7a4b3
gene <geneshuman@gmail.com>
2008-11-01 20:40:34 -0700

    refactoring done.  ready to add kernel variables

--- color.cu
+++
@@ -1,5 +0,0 @@
-extern "C" {
-  __device__ float4 rg_swizzle(float4 v){
-    return vec4(v.y, v.x, v.z, v.w);
-  }
-}
--- epimorph.py
+++ epimorph.py
@@ -5,33 +5,31 @@
 import sys
 import datetime
 
-from state import *
-from engine import *
-from interface import *
+from noumena.state import *
+from noumena.engine import *
+from noumena.renderer import *
 
-from logger import *
+from noumena.logger import *
 
-logger = Logger('log.txt', 'EP:  ')
-log = logger.log
-
-log("START - " + datetime.date.today().strftime("%m/%d/%y"))
+log("EP: START - " + datetime.date.today().strftime("%m/%d/%y"))
 
 #get variables
-app_vars = dict(tuple(cmd.split(':')) for cmd in sys.argv[1:] if cmd[0] != '_')
-state_vars = dict(tuple(cmd[1:].split(':')) for cmd in sys.argv[1:] if cmd[0] == '_')
+profile_vars = dict(tuple(cmd[1:].split(':')) for cmd in sys.argv[1:] if cmd[0] == '@')
+state_vars = dict(tuple(cmd[1:].split(':')) for cmd in sys.argv[1:] if cmd[0] == '$')
+other_vars = dict(tuple(cmd[1:].split(':')) for cmd in sys.argv[1:] if cmd[0] == '!')
 
 #initialize states
-manager = StateManager();
+manager = StateManager()
 
-state_name = app_vars.setdefault('state', 'default')
+state_name = other_vars.setdefault('state', 'default')
 state = manager.load_state(state_name, **state_vars)
 
-profile_name = app_vars.setdefault('profile', 'box1')
-profile = manager.load_profile(profile_name, **app_vars)
+profile_name = other_vars.setdefault('profile', 'box1')
+profile = manager.load_profile(profile_name, **profile_vars)
 
 #initialize
 engine = Engine(profile, state)
-interface = Interface(engine)
+renderer = Renderer(engine)
 
 #start state
-interface.start()
+renderer.start()
