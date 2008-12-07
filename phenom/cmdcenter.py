553c82d2904ac263c96769fd2a6a142d671907cb
gene <geneshuman@gmail.com>
2008-12-06 16:50:29 -0800

    shiz

--- phenom/cmdcenter.py
+++ phenom/cmdcenter.py
@@ -3,13 +3,14 @@ from phenom.console import *
 from phenom.keyboard import *
 from phenom.mouse import *
 
-from aer.datamanager import *
-
 import StringIO
 
 import sys
 
 class CmdCenter:
+
+    TPATH = "aer/t.epi"
+    TSEEDPATH = "aer/t_seed.epi"
 
     def __init__(self, state, renderer, engine):
         self.state, self.renderer, self.engine = state, renderer, engine
@@ -18,50 +19,82 @@ class CmdCenter:
         keyboard_handler = KeyboardHandler(self)
         console = Console(self)
         self.renderer.register_callbacks(keyboard_handler.keyboard, mouse_handler.mouse, mouse_handler.motion, console.render_console, console.console_keyboard)
-
-        self.datamanager = DataManager(self)    
+        
+        # load t
+        file = open(self.TPATH)
+        self.t = []
         self.t_idx = 0
+        for line in file.readlines():
+            if(line == "\n"):
+                continue
+            data = line.split(':')
+            data[0] = data[0].strip()
+            data[1] = data[1].strip()[1:-1].split(',')
+            self.t.append(data)
+        file.close()
+
+        # load t_seed
+        file = open(self.TSEEDPATH)
+        self.t_seed = []
         self.t_seed_idx = 0
+        for line in file.readlines():
+            if(line == "\n"):
+                continue
+            data = line.split(':')
+            data[0] = data[0].strip()
+            data[1] = data[1].strip()[1:-1].split(',')
+            self.t_seed.append(data)
+        file.close()
+
+
+    def load_t(self, idx):
+        idx = idx % len(self.t)
+        for i in range(0, 6):
+            print self.t[idx][1][i].replace('i', 'j')
+            self.state.zn[i] = complex(self.t[idx][1][i].replace('i', 'j'))
+        self.state.T = self.t[idx][0]
+        self.engine.compile_kernel()
+
+
+    def load_t_seed(self, idx):
+        idx = idx % len(self.t_seed)
+        for i in range(0, 3):
+            print self.t_seed[idx][1][i].replace('i', 'j')
+            self.state.zn[6 + i] = complex(self.t_seed[idx][1][i].replace('i', 'j'))
+        self.state.T_SEED = self.t_seed[idx][0]
+        self.engine.compile_kernel()
 
 
     def inc_t(self, idx):
         self.t_idx += idx
-        val = self.datamanager.t[self.t_idx]
-        self.state.T = val[0]
-        for line in val[1]:
-            exec(line)
-        self.engine.compile_kernel()
+        self.load_t(self.t_idx)
 
 
     def inc_t_seed(self, idx):
         self.t_seed_idx += idx
-        val = self.datamanager.t_seed[self.t_idx]
-        self.state.T_SEED = val[0]
-        for line in val[1]:
-            exec(line)
-        self.engine.compile_kernel()
+        self.load_t_seed(self.t_seed_idx)
 
 
     def cmd(self, code):
-        out = StringIO.StringIO()        
+        out = StringIO.StringIO()
+        
         sys.stdout = out
  
-        err = ""
+        res = ""
 
         try:
             exec code
         except:
-            err = traceback.format_exc().split("\n")[-2]
+            res = traceback.format_exc().split("\n")[-2]
 
 
         sys.stdout = sys.__stdout__
     
-        res = [out.getvalue(), err]
+        res = [out.getvalue(), res]
 
         out.close()
 
         return res
-
 
     def do(self):
         self.animator.do()
