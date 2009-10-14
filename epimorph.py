926ac36f86e2e877ac1264bcc8cd6eb32391f68a
gene shuman <gene@robots.windmill>
2009-10-14 14:04:42 -0700

    async main loop

--- epimorph.py
+++ epimorph.py
@@ -9,9 +9,12 @@ from noumena.renderer import *
 from noumena.engine import *
 from phenom.cmdcenter import *
 
+from common.runner import *
+
 from common.log import *
 set_log("EPIMORPH")
 info("Starting Epimorphism")
+
 
 # define & register exit handler
 def exit():
@@ -22,8 +25,10 @@ def exit():
 
 atexit.register(exit)
 
+
 # run unclutter to remove mouse pointer
-os.system("unclutter -idle 0.25 -jitter 1 -root&")
+os.system("unclutter -idle 0.25 -jitter 1 &")
+
 
 # initialize state/profile/context
 debug("Initializing state/profile/context")
@@ -43,35 +48,43 @@ context = manager.load_dict("context", context_vars["context"], **context_vars)
 state   = manager.load_dict("state", context.state, **parse_args("%"))
 profile = manager.load_dict("profile", context.profile, **parse_args("@"))
 
-# initialize modules
-debug("Initializing modules")
 
-renderer  = Renderer(state, profile, context)
-engine    = Engine(state, profile, context, renderer.pbo)
-cmdcenter = CmdCenter(state, renderer, engine, context)
-
-# create execution loop
-def inner_loop():
-    cmdcenter.do()
-    engine.do()
-    renderer.do()
-
-    if(context.exit):
-        renderer.__del__()
-        engine.__del__()
-        cmdcenter.__del__()
-        sys.exit()
-        os._exit()
-
-# set execution loop
-renderer.set_inner_loop(inner_loop)
-
-# define start function
-def start():
+# encapsulated for asynchronous execution
+def main():
     info("Starting main loop")
+
+    # initialize modules
+    debug("Initializing modules")
+
+    global renderer, engine, cmdcenter
+    renderer  = Renderer(state, profile, context)
+    engine    = Engine(state, profile, context, renderer.pbo)
+    cmdcenter = CmdCenter(state, renderer, engine, context)
+
+    # create execution loop
+    def inner_loop():
+        cmdcenter.do()
+        engine.do()
+        renderer.do()
+
+        if(context.exit):
+            renderer.__del__()
+            engine.__del__()
+            cmdcenter.__del__()
+            sys.exit()
+            os._exit()
+
+    # set execution loop & start
+    renderer.set_inner_loop(inner_loop)
     renderer.start()
 
     info("Main loop completed")
+
+
+# define start function
+def start():
+    async(main)
+
 
 # start
 if(context.autostart):
