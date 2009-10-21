24a76689a08e9dcaa4d6fa6419aff51f7b2e0242
gene shuman <gene@robots.windmill>
2009-10-20 20:59:58 -0700

    clean component manager & datamanager

--- phenom/componentmanager.py
+++ phenom/componentmanager.py
@@ -24,15 +24,15 @@ class ComponentManager(object):
 
 
     def set_component_indices(self):
+        ''' Given the current components in state, sets the
+            component index into datamanager '''
 
         for component_name in self.datamanager.component_names:
-            #print component_name
             idx = self.datamanager.component_names.index(component_name)
             val =  getattr(self.state, component_name.upper())
 
             try:
                 data = [elt[0] for elt in self.datamanager.components[component_name]]
-                #print data, val
                 self.component_idx[2 * idx] = data.index(val)
             except:
                 error("couldn't find index for: %s - %s" %(component_name, val))
@@ -40,18 +40,27 @@ class ComponentManager(object):
 
 
     def component_list(self):
+        ''' Returns a list of components '''
+
         return self.datamanager.component_names
 
 
     def print_components(self):
+        ''' Prints the currently listed components '''
+
         keys = self.datamanager.component_names
 
+        # print components
         for i in xrange(len(keys)) :
             component = getattr(self.state, keys[i])
             print i+1, ":", keys[i], "-", component, "-", self.datamanager.comment(keys[i], component)
 
 
     def inc_data(self, component_name, idx):
+        ''' Increments a component index '''
+        debug("Inc data: %s, %s" % (component_name, idx))
+
+        # abort if already switching
         if(self.switching_component):
             return
 
@@ -72,7 +81,10 @@ class ComponentManager(object):
 
 
     def can_switch_to_components(self, data):
+        ''' Checks if given components are loaded into kernel '''
+
         can_switch = True
+
         for component_name, val in data.items():
             idx_idx = self.datamanager.component_names.index(component_name)
             components = self.datamanager.components[component_name]
@@ -85,11 +97,10 @@ class ComponentManager(object):
         return can_switch
 
 
-    def set_kernel(self, name):
-        self.new_kernel = name
-
-
     def switch_components(self, data):
+        ''' Switches the system to the new components specified in data '''
+        debug("Switching components: %s" % str(data))
+
         if(len(data) == 0):
             return True
 
@@ -114,12 +125,15 @@ class ComponentManager(object):
             self.component_idx[2 * idx_idx + 1] = val_idx
             self.state.internal[idx_idx] = self.state.time
 
+        # if were only changing 1 component, show a message
         if(len(updates) == 1):
             self.cmdcenter.interface.renderer.flash_message("switching %s to: %s" % (updates.keys()[0], updates[updates.keys()[0]]["val"]))
 
+        # wait until interpolation is done
         while(self.state.time - self.state.internal[first_idx] < self.state.component_switch_time):
             time.sleep(0.1)
 
+        # update state
         for component_name, update in updates.items():
             val_idx = update["val_idx"]
             idx_idx = update["idx_idx"]
