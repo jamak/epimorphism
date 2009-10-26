1c5a19f1addfd74bd7a3128253a9fc9e1917c6b6
gene <gene@hyperanomalous.(none)>
2009-10-25 19:13:08 -0700

    in progress

---
+++ config/structs.py
@@ -0,0 +1,49 @@
+class MidiList(list):
+    ''' This is an internal class to add midi synchronization to
+        changes in parameters. '''
+
+    # maintain copy of origonal setter
+    old_set = list.__setitem__
+
+    def __setitem__(self, key, val):
+        # set value
+        self.old_set(key, val)
+
+        if(hasattr(self, "midi")):
+            self.midi.mirror(self, key)
+
+
+class State(object):
+    ''' Configuration parameters for generating Frames. '''
+
+    def __init__(self, **vars):
+        # update dict with migrated vars
+        self.__dict__.update(migrate(vars))
+
+        # create midi_lists
+        self.zn = MidiList(self.zn)
+        self.par = MidiList(self.par)
+
+
+class Profile(object):
+    ''' Configuration settings for the Engine. '''
+
+    def __init__(self, **vars):
+        # init
+        self.__dict__.update(vars)
+
+
+class Context(object):
+    ''' Configuration settings for the Interface. '''
+
+    def __init__(self, **vars):
+        # init
+        self.__dict__.update(vars)
+
+
+class Environment(object):
+    ''' Configuration settings for the application. '''
+
+    def __init__(self, **vars):
+        # init
+        self.__dict__.update(vars)
---
+++ phenom/script.py
@@ -0,0 +1,72 @@
+import sys
+import os.path
+
+from common.log import *
+set_log("SCRIPT")
+
+from common.runner import *
+from config.configmanager import *
+
+class Script(object):
+
+
+    def load_script(name, cmdcenter):
+        ''' Load and creates a script from disk '''
+
+        # create script
+        script = Script(cmdcenter)
+        script.name = name
+
+        # load events
+        script.events = ConfigManager.load_obj("script", name)
+
+        # return object
+        return script
+
+
+    def __init__(self, cmdcenter):
+        debug("Creating script")
+
+        self.cmdcenter = cmdcenter
+        self.events = []
+        self.name = None
+        self.current_idx = 0
+
+
+    def _execute(self):
+        ''' Internal execution loop '''
+
+        # main execution loop
+        while(self.current_idx < len(self.events) and not self.cmdcenter.env.exit):
+            if(self.cmdcenter.time() >= self.events[self.current_idx]["time"]):
+                self.cmdcenter.cmd(self.events[self.current_idx]["cmd"])
+                self.current_idx += 1
+
+
+    def start(self):
+        ''' Starts the script '''
+        debug("Start script")
+        async(_execute)
+
+
+    def add_event(self, event):
+        ''' Add an event to the collection of events '''
+
+        # compute insertion index
+        idx = [(i == 0 or event["time"] > self.events[i-1]["time"])
+               and (i == len(self.events) or event["time"] < self.events[i]["time"])
+               for i in xrange(len(self.events) + 1)].index(True)
+
+        # insert event
+        self.events.insert(idx, event)
+
+        # increment index if necessary
+        if(idx < self.current_idx): self.current_idx += 1
+
+
+
+    def save(self, name = None):
+        ''' Saves the script '''
+
+        # output events
+        ConfigManager.outp_dict("script", self.events, name)
